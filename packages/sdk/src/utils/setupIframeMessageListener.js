b0bb1f2e217e19c1b2a3a3baedd70d147a560e36
Sébastien Barbier <contact@sebastienbarbier.com>
2026-01-15 11:08:21 +0100

    Log unknown messages from ShellSDK

--- packages/sdk/src/utils/setupIframeMessageListener.js
+++ packages/sdk/src/utils/setupIframeMessageListener.js
@@ -8,29 +8,28 @@ export function setupIframeMessageListener() {
   }
 
   window.addEventListener('message', function(event) {
-    console.log('setupIframeMessageListener', event, window.location.pathname)
+
+    if (window.parent === window) {
+      return;
+    }
+
     // Handle SHELLUI_OPEN_MODAL messages
     if (event.data?.type === 'SHELLUI_OPEN_MODAL') {
-      // If we're in an iframe, propagate to parent
-      if (window.parent !== window) {
-        window.parent.postMessage({
-          type: 'SHELLUI_OPEN_MODAL',
-          payload: event.data.payload
-        }, '*');
-      }
+      window.parent.postMessage({
+        type: 'SHELLUI_OPEN_MODAL',
+        payload: event.data.payload
+      }, '*');
       // If we're at top level, the message will be handled by ModalProvider
     }
     // Handle SHELLUI_SETTINGS_UPDATED messages
     else if (event.data?.type === 'SHELLUI_SETTINGS_UPDATED') {
-      // If we're in an iframe, propagate to parent
-      if (window.parent !== window) {
-        window.parent.postMessage({
-          type: 'SHELLUI_SETTINGS_UPDATED',
-          payload: event.data.payload
-        }, '*');
-      } else {
-        console.log('setupIframeMessageListener DONE TO ROOT', event.data.payload, window.location.pathname)
-      }
+      window.parent.postMessage({
+        type: 'SHELLUI_SETTINGS_UPDATED',
+        payload: event.data.payload
+      }, '*');
+    } 
+    else {
+      console.warn('ShellSDK: unknown message', event.data)
     }
   });
 }
