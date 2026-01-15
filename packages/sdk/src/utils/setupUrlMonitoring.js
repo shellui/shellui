157637b29af560b1b737771f2da0531680276d2a
Sébastien Barbier <contact@sebastienbarbier.com>
2026-01-15 11:06:18 +0100

    Refactor SetupUrlMonitoring

--- packages/sdk/src/utils/setupUrlMonitoring.js
+++ packages/sdk/src/utils/setupUrlMonitoring.js
@@ -1,4 +1,33 @@
-import { handleUrlChange } from './handleUrlChange.js';
+/**
+ * Handles URL changes and notifies parent if the path has changed
+ * @param {Object} sdk - The SDK instance with currentPath property
+ */
+export function handleUrlChange(sdk) {
+  if (typeof window === 'undefined') {
+    return;
+  }
+  const newPath = window.location.pathname + window.location.search + window.location.hash;
+  if (newPath !== sdk.currentPath) {
+    sdk.currentPath = newPath;
+    if (typeof window === 'undefined') {
+      return;
+    }
+    const message = {
+      type: 'SHELLUI_URL_CHANGED',
+      payload: {
+        pathname: window.location.pathname,
+        search: window.location.search,
+        hash: window.location.hash,
+        fullPath: window.location.pathname + window.location.search + window.location.hash
+      }
+    };
+
+    // Send to parent frame
+    if (window.parent !== window) {
+      window.parent.postMessage(message, '*');
+    }
+  }
+}
 
 /**
  * Sets up listeners for various URL change events
