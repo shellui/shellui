95855f5e5686584b99a4a7f943bcf9522ab20990
Sebastien Barbier <contact@sebastienbarbier.com>
2026-01-23 12:28:33 +0100

    Change max height of setting panel

--- packages/core/src/features/layouts/DefaultLayout.tsx
+++ packages/core/src/features/layouts/DefaultLayout.tsx
@@ -187,7 +187,7 @@ const DefaultLayoutContent = ({ title, navigation }: DefaultLayoutProps) => {
       </div>
 
       <Dialog open={isOpen} onOpenChange={closeModal}>
-        <DialogContent className="max-w-4xl w-full h-[80vh] max-h-[720px] flex flex-col p-0 overflow-hidden">
+        <DialogContent className="max-w-4xl w-full h-[80vh] max-h-[680px] flex flex-col p-0 overflow-hidden">
           {modalUrl ? (
             <div className="flex-1" style={{ minHeight: 0 }}>
               <ContentView url={modalUrl} pathPrefix="settings" ignoreMessages={true} navItem={navigationItems.find(item => item.url === modalUrl)!} />
