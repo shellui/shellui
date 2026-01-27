aa7e164fef7c3b1b132d300389f472b1bfbcc381
Sebastien Barbier <contact@sebastienbarbier.com>
2026-01-27 12:50:51 +0700

    Add translation to Homeview

--- packages/core/src/components/HomeView.tsx
+++ packages/core/src/components/HomeView.tsx
@@ -1,4 +1,8 @@
+import { useTranslation } from 'react-i18next';
+
 export const HomeView = () => {
+  const { t } = useTranslation('common');
+
   return (
     <div style={{ 
       display: 'flex', 
@@ -8,8 +12,8 @@ export const HomeView = () => {
       flexDirection: 'column',
       color: '#666'
     }}>
-      <h1 style={{ margin: 0, fontSize: '2rem', fontWeight: 300 }}>Welcome to ShellUI</h1>
-      <p style={{ marginTop: '1rem', fontSize: '1.1rem' }}>Select a navigation item to get started.</p>
+      <h1 style={{ margin: 0, fontSize: '2rem', fontWeight: 300 }}>{t('welcome')}</h1>
+      <p style={{ marginTop: '1rem', fontSize: '1.1rem' }}>{t('getStarted')}</p>
     </div>
   );
 };
